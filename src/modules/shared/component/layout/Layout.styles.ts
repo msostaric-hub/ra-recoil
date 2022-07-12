c5c2af2106bb4595e27521499a3512009c1ca272
StjepanDzojic <stjepan@prototyp.digital>
2022-07-12 15:22:40 +0200

    Background fix

--- src/modules/shared/component/layout/Layout.styles.ts
+++ src/modules/shared/component/layout/Layout.styles.ts
@@ -3,11 +3,13 @@ import { css } from "@emotion/react";
 const backgroundPrimary = css`
   color: black;
   background: white;
+  min-height: 100vh;
 `;
 
 const backgroundSecondary = css`
   color: white;
   background: black;
+  min-height: 100vh;
 `;
 
 export const layoutStyles = { backgroundPrimary, backgroundSecondary };
